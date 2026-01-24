7d500b5a6b148fa74d4f159f4a17339c157b8d5f
Király Sándor <kiralysandor1986@gmail.com>
2026-01-24 16:16:46 +0100

    separate client logic from UI

--- client/src/pages/MeetingClient.tsx
+++ client/src/pages/MeetingClient.tsx
@@ -1,258 +1,33 @@
-import { useContext, useEffect, useRef, useState } from "react";
-import { DataContext } from "../providers/DataProvider";
-import { useNavigate, useParams } from "react-router";
-import { checkCamera, getCamera } from "../capture/getCamera";
-import { checkMicrophone, getMicrophone } from "../capture/getMicrophone";
-import socket from "../socket";
-import { createRecvTransport, createSendTransport } from "../mediasoup/utils";
-import { Device } from "mediasoup-client";
-import getRouterCapabilities from "../mediasoup/getRouterCapabilities";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { getCamera } from "../capture/getCamera";
+import { getMicrophone } from "../capture/getMicrophone";
 import type { Participant } from "../types/Participant";
 import StreamPlayer from "../components/StreamPlayer";
-import config from "../config";
 import { getScreen, checkScreenSupport } from "../capture/getScreen";
-import type { Transport } from "mediasoup-client/types";
 import { CameraIcon, ComputerDesktopIcon, MicrophoneIcon, PhoneArrowDownLeftIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import useClient from "../hooks/useClient";
 
 const MeetingClient = () => {
+    const [streamVolume, setStreamVolume] = useState(1);
+
     const {
         cameraStream,
-        joined,
         microphoneStream,
         setMicrophoneStream,
         setCameraStream,
-        nickname
-    } = useContext(DataContext);
+        nickname,
+        participants, setParticipants,
+        connected,
+        screenStream, setScreenStream,
+        hasAudio, hasVideo,
+        viewedParticipant, setViewedParticipant,
+        closeRef,
+        getStreamRef
+    } = useClient();
 
     const navigate = useNavigate();
-    const params = useParams();
 
-    const [participants, setParticipants] = useState<Record<string, Participant>>({});
-    const [device, setDevice] = useState<Device | null>(null);
-    const [sendStream, setSendStream] = useState<((stream: MediaStream, payloadId: number) => Promise<void>) | null>(null);
-    const [connected, setConnected] = useState(socket.connected);
-    const [screenStream, setScreenStream] = useState<MediaStream | null>(null);
-    const getStreamRef = useRef<(transportId: string, payloadId: number, onClose: Function) => Promise<{ stream: MediaStream, close: Function }>>(undefined)
-
-    const recTransportRef = useRef<Transport>(undefined);
-    const sendTransportRef = useRef<Transport>(undefined);
-
-    const [hasAudio, setHasAudio] = useState(false);
-    const [hasVideo, setHasVideo] = useState(false);
-
-    // check device
-    useEffect(() => {
-        checkCamera().then((has) => {
-            setHasVideo(has)
-        })
-
-        checkMicrophone().then((has) => {
-            setHasAudio(has)
-        })
-    }, [])
-
-    // 1. Navigation Guard
-    useEffect(() => {
-        if (connected) {
-            fetch(config.serverUrl + "/api/meeting/" + params.id, { method: "GET", headers: { "Content-Type": "application/json" } }).then(async (res) => {
-                if (res.status != 200) {
-                    console.error("Meeting does not exist")
-                    navigate("/")
-                }
-            })
-        }
-
-        if (!joined) {
-            navigate("/meeting/join/" + params.id);
-        }
-
-    }, [joined, navigate, params.id, connected]);
-
-    // 2. Setup Device
-    useEffect(() => {
-        if (!connected) return;
-
-        let isMounted = true;
-        const dev = new Device();
-
-        getRouterCapabilities().then(async (capabilities) => {
-            if (!isMounted) return;
-            await dev.load({ routerRtpCapabilities: capabilities });
-            setDevice(dev);
-        }).catch(console.error);
-
-        return () => { isMounted = false; };
-    }, [connected]);
-
-    // 3. Handle Receiving Streams
-    useEffect(() => {
-        if (!device || !connected) return;
-
-        let getStreamFunc: any;
-
-        createRecvTransport(socket, device, (transport) => {
-            recTransportRef.current = transport;
-        }).then((getstream) => {
-            getStreamFunc = getstream;
-            getStreamRef.current = getstream;
-            socket.emit("consumeReady");
-        });
-
-        const onNewProducer = async (transportId: string, payloadId: number) => {
-            if (!getStreamFunc) return;
-
-            const { stream } = await getStreamFunc(transportId, payloadId, () => {
-                // On Close: Remove stream from participant
-                setParticipants(prev => {
-                    const updated = { ...prev };
-                    if (!updated[transportId]) return prev; // Guard against unknown participant
-
-                    // Map payload IDs to specific stream properties
-                    if (payloadId === 1) updated[transportId].cameraStream = undefined;
-                    if (payloadId === 2) updated[transportId].microphoneStream = undefined;
-                    if (payloadId === 3) updated[transportId].streaming = false;
-                    if (payloadId === 3) updated[transportId].screenStream = undefined;
-                    if (payloadId === 4) updated[transportId].screenAudioStream = undefined;
-
-                    return { ...updated };
-                });
-            });
-            console.log("Add consumer: ", transportId, payloadId)
-            setParticipants(prev => {
-                const updated = { ...prev };
-                if (!updated[transportId]) return prev; // Guard against unknown participant
-
-                // Map payload IDs to specific stream properties
-                if (payloadId === 1) updated[transportId].cameraStream = stream;
-                if (payloadId === 2) updated[transportId].microphoneStream = stream;
-                // Only report that screenshare available
-                if (payloadId === 3) updated[transportId].streaming = true;
-                //if (payloadId === 4) updated[transportId].screenAudioStream = stream;
-
-                return { ...updated };
-            });
-        };
-
-        socket.on("newProducer", onNewProducer);
-
-        return () => { socket.off("newProducer", onNewProducer); };
-    }, [device, connected]);
-
-    const [transportId, setTransportId] = useState<string>()
-
-    // 4. Setup Send Transport
-    useEffect(() => {
-        if (!device || !connected) return;
-
-        createSendTransport(socket, device, (transport) => {
-            console.log("Send transport created");
-            setTransportId(transport.id);
-            sendTransportRef.current = transport;
-        }).then((sendstream) => {
-
-            setSendStream(() => sendstream);
-        });
-    }, [device, connected]);
-
-    // 5. Produce Local Streams (Camera/Mic)
-
-    useEffect(() => {
-        if (cameraStream && sendStream && sendTransportRef.current && connected) {
-            sendStream(cameraStream, 1).then(() => {
-                console.log("Sending camera stream")
-                socket.emit("addstream", 1)
-            })
-        }
-    }, [cameraStream, sendStream, sendTransportRef.current, connected]);
-
-    useEffect(() => {
-        if (microphoneStream && sendStream && sendTransportRef.current && connected) {
-            sendStream(microphoneStream, 2).then(() => {
-                console.log("Sending microphone stream")
-                socket.emit("addstream", 2)
-            })
-        }
-    }, [microphoneStream, sendStream, sendTransportRef.current, connected]);
-
-    // produce screen stream
-    useEffect(() => {
-        if (screenStream && sendStream) {
-            const vid = screenStream.getVideoTracks()[0]
-            const audio = screenStream.getAudioTracks()[0]
-            console.log(vid, audio)
-            sendStream(new MediaStream([vid]), 3).then(() => {
-                console.log("Added screen video")
-                socket.emit("addstream", 3)
-                if (audio) {
-                    sendStream(new MediaStream([audio]), 4).then(() => {
-                        console.log("Added screen audio")
-
-                        socket.emit("addstream", 4)
-                    })
-                }
-            })
-
-
-        }
-    }, [screenStream, sendStream]);
-
-    // 6. Participant Sync
-    useEffect(() => {
-        if (!transportId && !connected) {
-            return;
-        }
-
-        const onLeft = (transportId: string) => {
-            setParticipants(prev => {
-                const { [transportId]: deletedItem, ...remaining } = prev;
-                return remaining;
-            });
-        }
-
-        socket.on("participantLeft", onLeft)
-        socket.on("participants", (data: Record<string, Participant>) => setParticipants(data));
-        socket.on("newJoined", (data: Participant) => {
-            setParticipants(prev => ({ ...prev, [data.producerTransportId]: data }));
-        });
-
-        socket.emit("join", params.id, transportId, nickname);
-
-        return () => {
-            socket.off("participantLeft", onLeft);
-            socket.off("participants");
-            socket.off("newJoined");
-            socket.emit("leave");
-        };
-    }, [params.id, transportId, connected]);
-
-    // handle disconnect
-    useEffect(() => {
-        const onDisconnect = () => {
-            if (recTransportRef.current) {
-                recTransportRef.current.close()
-            }
-
-            if (sendTransportRef.current) {
-                sendTransportRef.current.close()
-            }
-
-            setDevice(null)
-            setConnected(false);
-            setParticipants({});
-        }
-
-        const onConnect = () => {
-            setConnected(true);
-        }
-
-        socket.on("connect", onConnect)
-
-        socket.on("disconnect", onDisconnect)
-
-        return () => {
-            socket.off("disconnect", onDisconnect)
-        }
-    }, [])
 
     // UI Handlers
     const toggleCamera = async () => {
@@ -309,10 +84,6 @@ const MeetingClient = () => {
         }
     }
 
-    const [viewedParticipant, setViewedParticipant] = useState<Participant | null>(null);
-    const [streamVolume, setStreamVolume] = useState(1);
-    const closeRef = useRef<{ closeVid: Function | undefined, closeAudio: Function | undefined }>({ closeVid: undefined, closeAudio: undefined })
-
     const viewStream = (p: Participant) => {
         setStreamVolume(1)
         if (p.streaming == true && getStreamRef.current != undefined) {
@@ -346,30 +117,6 @@ const MeetingClient = () => {
             })
         }
     }
-
-    // close all transports on leave
-    useEffect(() => {
-        return () => {
-            screenStream?.getTracks().forEach((track) => {
-                track.onended && track.onended(new Event("ended"))
-                track.stop()
-            })
-
-            cameraStream?.getTracks().forEach((track) => {
-                track.onended && track.onended(new Event("ended"))
-                track.stop()
-            })
-
-            microphoneStream?.getTracks().forEach((track) => {
-                track.onended && track.onended(new Event("ended"))
-                track.stop()
-            })
-
-
-            sendTransportRef.current && sendTransportRef.current.close();
-            recTransportRef.current && recTransportRef.current.close();
-        }
-    }, [])
 
 
     return (
