57a5bbcf28daa70e6aeffe61d9ed11dfed0f09cc
Király Sándor <kiralysandor1986@gmail.com>
2026-01-29 13:10:44 +0100

    set ideal resolution in camera capture

--- client/src/capture/getCamera.ts
+++ client/src/capture/getCamera.ts
@@ -4,8 +4,8 @@ const getCamera = async (): Promise<MediaStream | null> => {
     return new Promise((resolve) => {
         navigator.mediaDevices.getUserMedia({
             audio: false, video: {
-                width: { max: 1280 },
-                height: { max: 720 }
+                width: { max: 1280, ideal: 1280 },
+                height: { max: 720, ideal: 720 }
             }
         }).then((stream) => {
             resolve(stream)
