b4eebd94d15ecfaba3eeebc297fb4330256adb2a
Király Sándor <kiralysandor1986@gmail.com>
2026-03-30 17:42:30 +0200

    change color of speech indicator

--- client/src/components/Visualizer.tsx
+++ client/src/components/Visualizer.tsx
@@ -3,9 +3,7 @@ import { useAudioAnalyser } from "../hooks/useAudioAnalyser";
 const Visualizer = ({audioStream}: {audioStream: MediaStream}) => {
     const stat = useAudioAnalyser(audioStream);
 
-    return <div className="visualiser" style={{boxShadow: `inset 0 0px ${stat.speaking ? 80 : 0}px #39ff1466`}}>
-
-    </div>
+    return <div className="w-full h-full transition-all" style={{boxShadow: `inset 0 0px ${stat.speaking ? 80 : 0}px #155dfc`}}></div>
 }
 
 export default Visualizer;
