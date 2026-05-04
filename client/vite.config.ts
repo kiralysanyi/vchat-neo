d8a27565346c481f02231f6a1e11b3b0fc36bd0c
Király Sándor <kiralysandor1986@gmail.com>
2026-05-04 22:06:08 +0200

    basic pwa implementation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    manifest: {
+      name: "VChat-Neo",
+      short_name: "VChat",
+      start_url: "/",
+      description: "VChat-Neo by kiralysanyi"
+    }
+  })],
 })
